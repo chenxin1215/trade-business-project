7a8bf8d0c64f3a71c22d1aa4ed7c0f4f517a98ab
chenxin <154660165@qq.com>
2020-10-26 22:04:49 +0800

    实体fix

---
+++ trade-business-engine/src/main/java/com/cx/blog/dao/article/ArticleMapper.java
@@ -0,0 +1,9 @@
+package com.cx.blog.dao.article;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cx.blog.entity.article.Article;
+
+/**
+ * ArticleMapper继承基类
+ */
+public interface ArticleMapper extends BaseMapper<Article> {}
